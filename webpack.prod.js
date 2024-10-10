3127c903ab994a11f57a57854479552743ed54b4
AndresDev28 <andresdev28@hotmail.com>
2024-10-10 23:16:45 +0200

    feat: Webpack production build

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,4 +3,7 @@ const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'production', 
+  output: {
+    publicPath: '/https://github.com/AndresDev28/to-do-App',
+},
 });
